2b984ec7592c4864fff6c2a114f819bfe63f86fa
Yonatan Haddish <yonihailshn@gmail.com>
2022-02-03 13:14:24 -0500

    post delete and get routes created and is working well

--- routes/user.js
+++ routes/user.js
@@ -1,19 +1,54 @@
 const express= require('express');
+const User = require('../models/User');
 const router= express.Router();
 
-router.get('/home', (req, res) => {
-    res.json({
-        body: {
-            user: "User_01",
-            message: 'Home API'
-
-        }
-    });
+// getting all users
+router.get('/', async (req, res) => {
+    try {
+        const users= await User.find();
+        res.json(users);
+    }
+    catch(err) {
+        res.status(500).send();
+    }
 });
 
-router.post('/add', (req, res) => {
-    res.json(req.body);
-    console.log(req.body);
+// creating a new User
+router.post("/", async (req, res) => {
+    try {
+        const { name, email, password }= req.body;
+
+        const newUser= new User({ name, email, password });
+        
+        const savedUser= await newUser.save();
+
+        res.json(savedUser);
+    }
+    catch(err) {
+        res.status(500).send();
+    }
+});
+
+// deleting a user by a specific id
+router.delete("/:id", async (req, res) => {
+    try {
+        const userId= req.params.id;
+
+        if (!userId)
+            return res.status(400).json({errorMessge: 'User id not given. Please contact the developer.'});
+
+        const existingUser= await User.findById(userId);
+
+        if (!existingUser)
+            return res.status(400).json({errorMessge: 'No user with this id provided. Please contact the developer.'});
+
+        await existingUser.delete();
+
+        res.json(existingUser);
+    }
+    catch(err) {
+    res.status(500).send();
+    }
 });
 
 module.exports= router;
